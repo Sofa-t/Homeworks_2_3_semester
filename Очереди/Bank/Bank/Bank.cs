1b2323830d7d07428f63b1239e98cbe6e1276520
lamamalaga <tomchuk.01@gmail.com>
2020-12-07 15:52:07 +0500

    Очереди

---
+++ Очереди/Bank/Bank/Bank.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Bank
+{
+    class Client
+    {
+        public static Random Rnd = new Random();
+
+        public int TicketNumber { get; set; } //номер полученного талона электронной очереди
+        public int TimeToServeInMinutes { get; set; } //время, требуемое для обслуживания клиента        
+
+        public Client()
+        {
+            TicketNumber = Bank.LastTicketNumber++;
+            TimeToServeInMinutes = Rnd.Next(2, 6) * 5; // равновозможное время, необходимое для работы с клиентом - 10, 15, 20 или 25 минут
+        }
+    }
+
+    static class Bank
+    {
+        public class Window
+        {
+
+            public int WindowNumber; // номер окна
+            public Client CurrentClient { get; set; } //обслуживаемый в настоящий момент клиент
+            public DateTime NextClientTime; //время начала обслуживания следующего клиента
+
+            public void ChangeClient(Client newClient)
+            {
+                CurrentClient = newClient;
+                NextClientTime = Bank.CurrentTime.AddMinutes(newClient.TimeToServeInMinutes);
+            }
+
+            public void Anounce()
+            {
+                Console.WriteLine("{0,8:T} №{1,3} Окно {2}.", CurrentTime, CurrentClient.TicketNumber, WindowNumber+1);
+            }
+        }
+
+        
+
+        const int NumberOfWindows = 4; //количество окон для работы с клиентами в операционном зале банка 
+
+        public static Queue<Client> BankQueue { get; set; } //очередь клиентов, ожидающих обслуживания
+        public static Window[] Windows { get; set; } //массив окон для работы с клиентами
+        public static int LastTicketNumber { get; set; } //номер последнего выданного талона электронной очереди
+        public static DateTime CurrentTime; //текущее время
+        public static DateTime DayStart = new DateTime().AddHours(9); //банк начинает принимать новых клиентов в 9.00
+        public static DateTime DayEnd = new DateTime().AddHours(19); //банк заканчивает принимать новых клиентов в 19.00
+
+        /// <summary>
+        /// Метод находит первое свободное окно в операционном зале,
+        /// </summary>
+        /// <returns>
+        /// Возвращает окно, освободившееся раньше других, если таковое имеется.
+        /// Если нет - возвращает null
+        /// </returns>
+        public static Window GetEmptyWindow()
+        {
+            Window result = null;
+            foreach (var window in Windows)
+                if (window.NextClientTime <= CurrentTime &&
+                        (result == null || window.NextClientTime < result.NextClientTime))
+                    result = window;
+            return result;
+        }
+
+        static Bank()
+        {
+            BankQueue = new Queue<Client>();
+            CurrentTime = DayStart;
+            LastTicketNumber = 0;
+            Windows = new Window[NumberOfWindows];
+            for (var i = 0; i < NumberOfWindows; i++)
+                Windows[i] = new Window() { WindowNumber = i, NextClientTime = CurrentTime };
+        }
+
+    }
+}
---
+++ Очереди/Bank/Bank/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Bank
+{
+    class Program
+    {
+
+        static void Main()
+        {
+            double lambda = 15; //интенсивность потока - среднее количество клиентов в час
+            int timeIntervalInSeconds = 5; //промежуток, в течение которого проверяется, пришел ли новый клиент     
+
+            //приход в банк клиентов моделируется на основе простейшего потока случайных событий
+            double p = Math.Pow(Math.E, -lambda * timeIntervalInSeconds / (60*60)); //вероятность того, что в заданный временной интервал клиент не придет
+
+            for (DateTime timer = Bank.DayStart; timer < Bank.DayEnd; timer = timer.AddSeconds(timeIntervalInSeconds))
+            {
+                //Увеличиваем время на timeIntervalInSeconds
+                Bank.CurrentTime = Bank.CurrentTime.AddSeconds(timeIntervalInSeconds);
+                                
+                //Пришел ли новый клиент в интервал timeIntervslInSeconds
+                if (Client.Rnd.NextDouble() > p) //клиент пришел
+                {
+                    ... //ставим нового клиента в очередь на обслуживание
+                }
+
+                ServeClient();
+            }
+
+            while (Bank.BankQueue.Count > 0)
+            {
+                ... // продолжаем обслуживать клиентов, которые встали в очередь до закрытия банка
+            }
+
+            Console.ReadKey();
+        }
+
+        static void ServeClient()
+        {
+            var freeWindow = Bank.GetEmptyWindow(); 
+
+            if (freeWindow != null && Bank.BankQueue.Count > 0) 
+            {
+                ... // если  есть свободное окно и есть клиенты в очереди, это окно берет очередного клиента на обслуживание и объявляет об этом
+            }
+        }
+
+    }
+}
