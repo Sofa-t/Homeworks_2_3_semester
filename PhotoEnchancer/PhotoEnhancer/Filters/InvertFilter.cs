96d9840ef0457a43281e82d3807e2e690d817b00
lamamalaga <tomchuk.01@gmail.com>
2020-12-07 16:39:15 +0500

    Фильтр инверсия

---
+++ PhotoEnchancer/PhotoEnhancer/Filters/InvertFilter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace PhotoEnhancer
+{
+    class InvertFilter : PixelFilter
+    {
+        public InvertFilter() : base(new EmptyParameters()) { }
+
+        public override string ToString()
+        {
+            return "Инверсия";
+        }
+
+        public override Pixel ProcessPixel(Pixel originalPixel, IParameters parameters)
+        {
+
+            var newR = 1 - originalPixel.R;
+            var newG = 1 - originalPixel.G;
+            var newB = 1 - originalPixel.B;
+
+            return new Pixel(newR, newG, newB);
+        }
+
+    }
+   
+}
