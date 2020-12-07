99386a1ea52d914e8c3d06230c31e41b5d776c15
lamamalaga <tomchuk.01@gmail.com>
2020-12-07 15:54:36 +0500

    ООП Задание 2

---
+++ HRLibrary/HRLibrary/HRLibrary.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRLibrary
+{
+
+    public enum CommodityUnits
+    {
+        Pieces,
+        Kilogram,
+        Tonne,
+        Packaging
+    }
+    public class Commodity
+    {
+
+        // Создаем, описанные в документе, соответствующие свойства 
+        public string ArticleNumber { get; private set; }
+        public string Name { get; set; }
+        public double WholesalePrice { get; set; }
+        public double RetailPrice { get; set; }
+        public CommodityUnits CommodityUnit { get; set; }
+        public int Count { get; set; }
+        public string Description { get; set; }
+
+        // Конструктор для нашего класса
+        public Commodity(string name, string articleNumber, CommodityUnits commodityUnit,
+            double wholeselePrice, double retailPrice)
+        {
+            Name = name;
+            ArticleNumber = articleNumber;
+            WholesalePrice = wholeselePrice;
+            RetailPrice = retailPrice;
+            CommodityUnit = commodityUnit;
+        }
+
+        // Переопределяем метод ToString(). Он будет выводить название и артикул продукта 
+        public override string ToString()
+        {
+            return $"{Name} {ArticleNumber}";
+        }
+
+        // Метод для вывода всей информации.
+        public virtual void PrintInfo()
+        {
+            Console.WriteLine(this);//Сначала мы ввыводим имя и фамилию посредством метода ToString. "this" означает,
+            //что метод ToString применяется к объекту, с которым мы работаем в данный момент
+
+            var commodityUnit = " ";
+
+            // Разворачиваем наш enum 
+            switch (CommodityUnit)
+            {
+                case CommodityUnits.Kilogram:
+                    commodityUnit = "кг";
+                    break;
+                case CommodityUnits.Packaging:
+                    commodityUnit = "пак";
+                    break;
+                case CommodityUnits.Pieces:
+                    commodityUnit = "шт";
+                    break;
+                case CommodityUnits.Tonne:
+                    commodityUnit = "т";
+                    break;
+            }
+            // Выводим остальную информацию
+            Console.WriteLine($"Оптовая цена за единицу товара: {WholesalePrice}руб. за {commodityUnit}," +
+                $" Розничная цена за единицу товара {RetailPrice}руб. за {commodityUnit}," +
+                $" Описание: {Description} На складе: {Count}");
+        }
+    }
+}
---
+++ HRLibrary/HRUnitTests/UnitTest.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HRLibrary;
+using System.IO;
+    
+namespace HRUnitTests
+{
+    [TestClass]
+    public class CommodityUnitTest
+    {
+        // В тестовом классе все работает точно так же, как и в методичке
+
+
+        // Метод, тестирующий конструктор. Создаем объект Commodity. Проверяем, правильно ли он работает.
+        [TestMethod]
+        public void ConstructorTestMethod()
+        {
+            var product = CreateCommodity();
+
+            Assert.AreEqual("Кресло", product.Name);
+            Assert.AreEqual("Для офиса", product.ArticleNumber);
+            Assert.AreEqual(CommodityUnits.Pieces, product.CommodityUnit);
+            Assert.AreEqual(5000, product.WholesalePrice);
+            Assert.AreEqual(6540, product.RetailPrice);
+        }
+
+        // Проверяем метод ToString().
+        [TestMethod]
+        public void ToStringTestMethod()
+        {
+            var product = CreateCommodity();
+            Assert.AreEqual("Кресло Для офиса", product.ToString());
+        }
+
+        // Проверяем метод PrintInfo().
+        [TestMethod]
+        public void PrintInfoTestMethod()
+        {
+            //Создаем два объекта firstProduct и secondProduct
+            var firstProduct = CreateCommodity();
+            var secondProduct = new Commodity("Стол", "Для кухни", CommodityUnits.Pieces, 2000, 2300)
+            {
+                Count = 100,
+                Description = "Обычный обеденный стол для кафе, кухни, дачи. Столешница- пластик." +
+                " Разные варианты расцветок. Простой и недорогой. Самый минимум." +
+                " Ножки стола могут быть цилиндрическими-хром."
+            };
+
+            //Заносим правильные данные в массив consoleOut
+            var consoleOut = new[]
+            {
+                "Кресло Для офиса",
+
+                "Оптовая цена за единицу товара: 5000руб. за шт," +
+                " Розничная цена за единицу товара 6540руб. за шт," +
+                " Описание: Современое и удобное кресло «на полозьях». Стильное и оригинальное," +
+                " на стальном основании, оно позволит Вашим сотрудникам работать в офисе в комфортных условиях." +
+                " Прекрасный вариант для домашнего использования, особенно в случаях с покрытием пола из ламината."+
+                " На складе: 100",
+
+                "Стол Для кухни",
+
+                "Оптовая цена за единицу товара: 2000руб. за шт," +
+                " Розничная цена за единицу товара 2300руб. за шт," +
+                " Описание: Обычный обеденный стол для кафе, кухни, дачи. Столешница- пластик." +
+                " Разные варианты расцветок. Простой и недорогой. Самый минимум." +
+                " Ножки стола могут быть цилиндрическими-хром."+
+                " На складе: 100"
+            };
+
+            // Т.к. PrintInfo выводит данные на консоль,
+            // мы перенаправим вывод в файл.
+            TextWriter oldOut = Console.Out;
+
+            using (FileStream file = new FileStream("test.txt", FileMode.Create))
+            {
+                using (TextWriter writer = new StreamWriter(file))
+                {
+                    Console.SetOut(writer);
+                    firstProduct.PrintInfo();
+                    secondProduct.PrintInfo();
+                }
+            }
+
+            Console.SetOut(oldOut);
+            var i = 0;
+
+            //После этого считаем все данные из файла и сверим с тем, что было в массиве consoleOut
+            foreach (var line in File.ReadLines("test.txt"))
+                Assert.AreEqual(consoleOut[i++], line);
+
+            // Удаляем файл
+            File.Delete("test.txt");
+        }
+
+        // этот метод существует лишь для того, чтобы в тестовых методах не писать одно и то же несколько раз.
+        public Commodity CreateCommodity()
+        {
+            return new Commodity("Кресло", "Для офиса", CommodityUnits.Pieces, 5000, 6540)
+            { Count = 100, Description = "Современое и удобное кресло «на полозьях». Стильное и оригинальное," +
+            " на стальном основании, оно позволит Вашим сотрудникам работать в офисе в комфортных условиях." +
+            " Прекрасный вариант для домашнего использования, особенно в случаях с покрытием пола из ламината." };
+        }
+    }
+}
