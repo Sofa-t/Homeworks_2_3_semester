71368f6eefeb790a09121469a0deb13e7b755f96
Kikaboy <56254100+Kikaboy@users.noreply.github.com>
2021-01-15 14:51:50 +0500

    HometaskPE#2

---
+++ PhotoEnhancer/PhotoEnhancer/Filters/Transformery/ReversePerspParameters.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoEnhancer
+{
+    class ReversePerspParameters : IParameters
+    {
+        public double Squish { get; set; }
+
+        public ParameterInfo[] GetDescription()
+        {
+            return new[]
+            {
+                new ParameterInfo() {
+                    Name = "Сужаемая часть(в процентах)",
+                    MinValue = 0,
+                    MaxValue = 100,
+                    DefaultValue = 100,
+                    Increment = 5
+                    }
+            };
+        }
+
+        public void SetValues(double[] values)
+        {
+            Squish = values[0];
+        }
+    }
+}
+
---
+++ PhotoEnhancer/PhotoEnhancer/Filters/Transformery/ReversePerspectiveTransformer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace PhotoEnhancer
+{
+    class ReversePerspectiveTransformer : ITransformer<ReversePerspParameters>
+    {
+        public Size ResultSize { get; set; }
+
+        Size originalSize;
+        double Squish;
+
+        public void Initialize(Size size, ReversePerspParameters parameters)
+        {
+            originalSize = size;
+            Squish = parameters.Squish;
+            ResultSize = originalSize;
+        }
+
+        public Point? MapPoint(Point point)
+        {
+            {
+                double x = point.X;
+                double y = point.Y;
+                double variable = 1 - y / ResultSize.Height;
+                double percent = 100 / (100 - Squish + Squish * variable);
+                x = x * percent + (1 - percent) * ResultSize.Width / 2;
+
+                if (x < 0 || x > originalSize.Width - 1)
+                    return null;
+
+                return new Point((int)x, (int)y);
+            }
+
+        }
+
+    } 
+    
+}
+
+
